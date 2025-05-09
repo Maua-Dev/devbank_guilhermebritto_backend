c0453132f8f7c3f11c7d431d53c26fc21b53b8b8
Guilherme Britto <guibrittotava@gmail.com>
2025-05-09 18:35:15 -0300

    codigo de validacao do usuario 'completo'

--- src/app/entities/user.py
+++ src/app/entities/user.py
@@ -1,4 +1,6 @@
 from typing import Tuple
+import re
+from ..errors.entity_errors import ParamNotValidated
 
 class User:
     name: str
@@ -9,14 +11,23 @@ class User:
     def __init__(self, name: str, agency: str, account: str, current_balance: float):
         validar_nome = self.validate_name(name)
         if validar_nome[0] == False:
-            raise Exception("name", validar_nome[1])
+            raise ParamNotValidated("name", validar_nome[1])
         self.name = name
 
         validar_agencia = self.validate_agency(agency)
         if validar_agencia[0] == False:
-            raise Exception("agency", validar_agencia[1])
+            raise ParamNotValidated("agency", validar_agencia[1])
         self.agency = agency
         
+        validar_conta = self.validate_account(account)
+        if validar_conta[0] == False:
+            raise ParamNotValidated("Account", validar_conta[1])
+        
+        validar_saldo = self.validate_current_balance(current_balance)
+        if validar_saldo[0] == False:
+            raise ParamNotValidated("Current Balance", validar_saldo[1])
+        
+
     @staticmethod
     def validate_name(name) -> Tuple[bool, str]:
         if name == None:
@@ -36,14 +47,28 @@ class User:
             raise Exception("A agência deve conter 4 dígitos!")
 
         return (True, "Agência válida")
+    
     @staticmethod
-    def validate_account():
-        pass
+    def validate_account(account) -> Tuple[bool, str]:
+        if len(account) != 7:
+            return(False, "Conta inválida")
+        
+        if account != re.match("^([0,9]{5}\-[0,9]{1}})$"):
+            return (False, "Conta não segue o padrão XXXXX-X")
+
+        return (True, "Conta válida")
+
 
     @staticmethod
-    def validate_current_balance():
-        pass
+    def validate_current_balance(current_balance) -> Tuple[bool, str]:
+        if type(current_balance) != float:
+            return (False, "Saldo tem que ser float")
+        
+        if current_balance < 0:
+            return (False, "Saldo não pode ser negativo")
+        
+        return (True, "Saldo válido")
 
 
-teste = User("oiiii", "4343", "aaaa", 1000)
+teste = User("oiiii", "4343", "43434-3", 1000)
 print(teste)
